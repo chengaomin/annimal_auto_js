6283d7abda2d3d1dfc1b5a334bfb908663a38356
chengaomin <445462004@qq.com>
2021-01-19 22:54:36 +0800

    Add files via upload

---
+++ click.js
@@ -0,0 +1,47 @@
+// 点宣传
+setInterval(()=>{
+    press(961, 1782, 1);
+}, 100);
+
+
+// 点菜
+setInterval(()=>{
+    press(396, 712, 1);
+}, 1000);
+setInterval(()=>{
+    press(629, 712, 1);
+}, 1000);
+setInterval(()=>{
+    press(870, 712, 1);
+}, 1000);
+setInterval(()=>{
+    press(396, 1036, 1);
+}, 1000);
+setInterval(()=>{
+    press(629, 1036, 1);
+}, 1000);
+setInterval(()=>{
+    press(870, 1036, 1);
+}, 1000);
+
+
+
+// 捡小鱼
+setInterval(()=>{
+    press(422, 867, 1);
+}, 5000);
+setInterval(()=>{
+    press(663, 867, 1);
+}, 5000);
+setInterval(()=>{
+    press(935, 867, 1);
+}, 5000);
+setInterval(()=>{
+    press(422, 1188, 1);
+}, 5000);
+setInterval(()=>{
+    press(663, 1188, 1);
+}, 5000);
+setInterval(()=>{
+    press(935, 1188, 1);
+}, 5000);
